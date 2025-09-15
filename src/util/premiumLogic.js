2e1aaba3875937054d51aea49a813bfc12e7474b
Cong Lyu <lyu0061@flinders.edu.au>
2025-09-15 14:59:11 +0930

    Create premium check function.

---
+++ src/util/premiumLogic.js
@@ -0,0 +1,13 @@
+function isTodayOrFuture(dateStr) {
+  const today = new Date()
+  const yyyy = today.getFullYear()
+  const mm = String(today.getMonth() + 1).padStart(2, '0')
+  const dd = String(today.getDate()).padStart(2, '0')
+  const todayStr = `${yyyy}-${mm}-${dd}`
+
+  return dateStr >= todayStr
+}
+
+module.exports = {
+  isTodayOrFuture
+}
