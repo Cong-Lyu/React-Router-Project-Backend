56ce31471d5682119642880b4b98a5266ec08dfe
Cong Lyu <lyu0061@flinders.edu.au>
2025-09-26 18:11:58 +0930

    Create user uploads route.

--- reactRouterBackendServer.js
+++ reactRouterBackendServer.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const routerPicture = require('./src/routes/publicPictureRoutes.js')
 const routerJwt = require('./src/routes/jwtVerifyRoutes.js')
 const routerPremium = require('./src/routes/premiumRoutes.js')
+const routerUpload = require('./src/routes/getCredential.js')
 // const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
@@ -26,6 +27,7 @@ reactRouterServer.use(express.urlencoded({ extended: true }));
 reactRouterServer.use('/api/public', routerPicture)
 reactRouterServer.use('/api/jwt', routerJwt)
 reactRouterServer.use('/api/premium', routerPremium)
+reactRouterServer.use('/api/upload', routerUpload)
 
 reactRouterServer.listen(port, () => {
   console.log(`Now react Router SERVER is listening request at ${port}......`)
