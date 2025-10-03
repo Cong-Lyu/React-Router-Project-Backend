a80e1ec59079e260d9e34ec0cdc994b3a4fe94a4
Cong Lyu <lyu0061@flinders.edu.au>
2025-10-03 16:17:02 +0930

    Change key to process environment variables.

--- src/util/jwtGenerator.js
+++ src/util/jwtGenerator.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const privateKey = fs.readFileSync(path.join(__dirname, '../../private.pem'), 'utf8')
+const privateKey = process.env.MY_PRIVATE_KEY
 const publicKey = fs.readFileSync(path.join(__dirname, '../../public.pem'), 'utf8')
 const clientId = `921371467501-6a2oag4udjf0a2u1db7a4n7teuk26q63.apps.googleusercontent.com`
 const { OAuth2Client } = require('google-auth-library')
