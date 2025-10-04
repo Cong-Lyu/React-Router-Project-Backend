57c3e7715bc407135a044fd6ee8e401961f98425
Cong Lyu <lyu0061@flinders.edu.au>
2025-10-04 14:20:24 +0930

    fix login bug

--- src/middleware/jwtVerify.js
+++ src/middleware/jwtVerify.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 require('dotenv').config()
-const privateKey = process.env.MY_PRIVATE_KEY || fs.readFileSync(path.join(__dirname, '../../private.pem'), 'utf8')
-const publicKey = process.env.MY_PUBLIC_KEY || fs.readFileSync(path.join(__dirname, '../../public.pem'), 'utf8')
+const privateKey = process.env.MY_PRIVATE_KEY ? process.env.MY_PRIVATE_KEY.replace(/\\n/g, '\n') : fs.readFileSync(path.join(__dirname, '../../private.pem'), 'utf8')
+const publicKey = process.env.MY_PUBLIC_KEY ? process.env.MY_PUBLIC_KEY.replace(/\\n/g, '\n') : fs.readFileSync(path.join(__dirname, '../../public.pem'), 'utf8')
 const clientId = `921371467501-6a2oag4udjf0a2u1db7a4n7teuk26q63.apps.googleusercontent.com`
 const { OAuth2Client } = require('google-auth-library')
 const client = new OAuth2Client(clientId)
